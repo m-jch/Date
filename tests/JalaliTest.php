7dc2d40c064988cd3d194475c05ed88a9df0721a
mohsen shafiei <mohsen.jch@gmail.com>
2017-12-04 12:25:24 +0330

    Force tests to use UTC timezone - resolve #3

--- tests/JalaliTest.php
+++ tests/JalaliTest.php
@@ -11,16 +11,16 @@ class JalaliTest extends TestCase
 
     public function testDiffInYears()
     {
-        $date1 = new Jalali('1390-05-06');
-        $date2 = new Jalali('1395-12-07');
+        $date1 = new Jalali('1390-05-06', 'UTC');
+        $date2 = new Jalali('1395-12-07', 'UTC');
 
         $this->assertSame(5, $date2->diffInYears($date1));
     }
 
     public function testDiffInYearsBaseOnDifference()
     {
-        $date1 = new Jalali('1390-05-06');
-        $date2 = new Jalali('1395-12-07');
+        $date1 = new Jalali('1390-05-06', 'UTC');
+        $date2 = new Jalali('1395-12-07', 'UTC');
 
         $this->assertSame(-5, $date2->diffInYears($date1, false));
     }
@@ -31,28 +31,28 @@ class JalaliTest extends TestCase
 
     public function testTimestampInitialize()
     {
-        $date = new Jalali(1466664181);
+        $date = new Jalali(1466664181, 'UTC');
 
-        $this->assertSame('1395-04-03 11:13:01', $date->format('Y-m-d H:i:s'));
+        $this->assertSame('1395-04-03 06:43:01', $date->format('Y-m-d H:i:s'));
     }
 
     public function testTimestampCreator()
     {
-        $date = Jalali::createFromTimestamp(1466664181);
+        $date = Jalali::createFromTimestamp(1466664181, 'UTC');
 
-        $this->assertSame('1395-04-03 11:13:01', $date->format('Y-m-d H:i:s'));
+        $this->assertSame('1395-04-03 06:43:01', $date->format('Y-m-d H:i:s'));
     }
 
     public function testTimestampGet()
     {
-        $date = new Jalali(1466664492);
+        $date = new Jalali(1466664492, 'UTC');
 
         $this->assertSame(1466664492, $date->getTimestamp());
     }
 
     public function testTimestampFormat()
     {
-        $date = new Jalali(1466664492);
+        $date = new Jalali(1466664492, 'UTC');
 
         $this->assertSame('1466664492', $date->format('U'));
     }
